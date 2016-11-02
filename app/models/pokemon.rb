3d66e8cca87131541ac975be9eb4f3d4454dffed
Caleb Studuent <calebs11@berkeley.edu>
2016-11-02 03:14:29 -0700

    I am a Pokemon master.

---
+++ app/models/pokemon.rb
@@ -0,0 +1,6 @@
+class Pokemon < ActiveRecord::Base
+	belongs_to :trainer 
+	validates :name, presence: true, :uniqueness => {:scope => :name}
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  patch 'capture' => 'pokemon#capture', as: 'capture'
+  patch 'damage' => 'pokemon#damage', as: 'damage'
+  get 'newpokemon' => 'pokemon#new', as: 'newpokemon'
+  post 'createpokemon' => 'pokemon#create', as: 'createpokemon'
   devise_for :trainers
   resources :trainers
 end
